b244e99c23c59ce3409aed0743b785a6a5a46182
marcgine <65180746+marcgine@users.noreply.github.com>
2021-03-24 18:31:40 +0100

    Primer commit activitat 5!

--- js/matrius_MVB.js
+++ js/matrius_MVB.js
@@ -3,10 +3,14 @@ let llistaElements = document.getElementById("llistaElements");
 
 function afegirElementAColleccioMVB() {
   let elementAAfegir = document.getElementById("elementAAfegir");
-  colleccioElements.push(elementAAfegir.value);
-  alert("Element " + elementAAfegir.value + " afegit correctament " + "a la posició " + colleccioElements.indexOf(elementAAfegir.value))
-  document.getElementById('elementAAfegir').value = null;
-}
+  if (elementAAfegir.value == "") {
+    alert("L'objecte ha de tenir nom!!!");
+  } else {
+    colleccioElements.push(elementAAfegir.value);
+    alert("Element " + elementAAfegir.value + " afegit correctament " + "a la posició " + colleccioElements.indexOf(elementAAfegir.value));
+    document.getElementById('elementAAfegir').value = null;
+  }
+};
 
 function mostrarElementsDeLaColleccio() {
   let cadLlistaElem = "";
